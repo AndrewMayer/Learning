343997d2ead2a0f2f44135f4fc8ec108936278e5
andrewmayer <andrew@levelupdesign.com>
2019-06-08 15:43:25 -0700

    Version with drag handles added

--- beautifulDnD/task-app/src/index.js
+++ beautifulDnD/task-app/src/index.js
@@ -8,22 +8,8 @@ import Column from './column';
 class App extends React.Component {
   state = initialData;
 
-  onDragStart = () => {
-    document.body.style.color = 'gold';
-    document.body.style.transition = 'background-color 0.2s ease';
-  };
-
-  onDragUpdate = update => {
-    const { destination } = update;
-    const opacity = destination
-      ? destination.index / Object.keys(this.state.tasks).length
-      : 0;
-    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
-  };
   onDragEnd = result => {
     const { destination, source, draggableId } = result;
-    document.body.style.color = 'inherit';
-    document.body.style.backgroundColor = 'inherit';
     if (!destination) {
       return;
     }
@@ -57,11 +43,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <DragDropContext
-        onDragStart={this.onDragStart}
-        onDragUpdate={this.onDragUpdate}
-        onDragEnd={this.onDragEnd}
-      >
+      <DragDropContext onDragEnd={this.onDragEnd}>
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
