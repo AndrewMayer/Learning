aba327ef8b0968d759eea53339a715b43a39b244
andrewmayer <andrew@levelupdesign.com>
2019-06-13 13:51:24 -0700

    Updated variables: start-finish home-foreign

--- beautifulDnD/task-app/src/index.js
+++ beautifulDnD/task-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
 import styled from 'styled-components';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
@@ -13,24 +13,37 @@ const Container = styled.div`
 class App extends React.Component {
   state = initialData;
 
-  onDragStart = () => {
-    document.body.style.color = 'gold';
-    document.body.style.transition = 'background-color 0.2s ease';
-  };
+  // onDragStart = () => {
+  //   document.body.style.color = 'gold';
+  //   document.body.style.transition = 'background-color 0.2s ease';
+  // };
 
-  onDragUpdate = update => {
-    const { destination } = update;
-    const opacity = destination
-      ? destination.index / Object.keys(this.state.tasks).length
-      : 0;
-    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
-  };
+  // onDragUpdate = update => {
+  //   const { destination } = update;
+  //   const opacity = destination
+  //     ? destination.index / Object.keys(this.state.tasks).length
+  //     : 0;
+  //   document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  // };
+
   onDragEnd = result => {
-    const { destination, source, draggableId } = result;
-    document.body.style.color = 'inherit';
-    document.body.style.backgroundColor = 'inherit';
+    const { destination, source, draggableId, type } = result;
+    // document.body.style.color = 'inherit';
+    // document.body.style.backgroundColor = 'inherit';
     if (!destination) {
       return;
+    }
+
+    if (type === 'column') {
+      const newColumnOrder = Array.from(this.state.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+
+      const newState = {
+        ...this.state,
+        columnOrder: newColumnOrder
+      };
+      this.setState(newState);
     }
 
     if (
@@ -39,24 +52,43 @@ class App extends React.Component {
     ) {
       return;
     }
-    const start = this.state.columns[source.droppableId];
-    const finish = this.state.columns[destination.droppableId];
+    console.log('source is');
+    console.log(source);
+    const home = this.state.columns[source.droppableId];
+    console.log('Home is');
+    console.log(home);
+    const foreign = this.state.columns[destination.droppableId];
+    // const start = this.state.columns[source.droppableId];
+    // const finish = this.state.columns[destination.droppableId];
 
-    if (start === finish) {
-      const newTaskIds = Array.from(start.taskIds);
+    if (home === foreign) {
+      const newTaskIds = Array.from(home.taskIds);
       newTaskIds.splice(source.index, 1);
       newTaskIds.splice(destination.index, 0, draggableId);
 
-      const newColumn = {
-        ...start,
+      // const newColumn = {
+      //   ...start,
+      //   taskIds: newTaskIds
+      // };
+
+      const newHome = {
+        ...home,
         taskIds: newTaskIds
+      };
+
+      const foreignTaskIds = Array.from(foreign.taskIds);
+      foreignTaskIds.splice(destination.index, 0, draggableId);
+      const newForeign = {
+        ...foreign,
+        taskIds: foreignTaskIds
       };
 
       const newState = {
         ...this.state,
         columns: {
           ...this.state.columns,
-          [newColumn.id]: newColumn
+          [newHome.id]: newHome,
+          [newForeign.id]: newForeign
         }
       };
 
@@ -65,26 +97,26 @@ class App extends React.Component {
     }
 
     //Moving from one column to another
-    const startTaskIds = Array.from(start.taskIds);
-    startTaskIds.splice(source.index, 1);
-    const newStart = {
-      ...start,
-      taskIds: startTaskIds
+    const homeTaskIds = Array.from(home.taskIds);
+    homeTaskIds.splice(source.index, 1);
+    const newHome = {
+      ...home,
+      taskIds: homeTaskIds
     };
 
-    const finishTaskIds = Array.from(finish.taskIds);
-    finishTaskIds.splice(destination.index, 0, draggableId);
-    const newFinish = {
-      ...finish,
-      taskIds: finishTaskIds
+    const foreignTaskIds = Array.from(foreign.taskIds);
+    foreignTaskIds.splice(destination.index, 0, draggableId);
+    const newForeign = {
+      ...foreign,
+      taskIds: foreignTaskIds
     };
 
     const newState = {
       ...this.state,
       columns: {
         ...this.state.columns,
-        [newStart.id]: newStart,
-        [newFinish.id]: newFinish
+        [newHome.id]: newHome,
+        [newForeign.id]: newForeign
       }
     };
     this.setState(newState);
@@ -93,16 +125,31 @@ class App extends React.Component {
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-        <Container>
-          {this.state.columnOrder.map(columnId => {
-            const column = this.state.columns[columnId];
-            const tasks = column.taskIds.map(
-              taskId => this.state.tasks[taskId]
-            );
+        <Droppable
+          droppableId="all-columns"
+          direction="horizontal"
+          type="column"
+        >
+          {provided => (
+            <Container {...provided.droppableProps} ref={provided.innerRef}>
+              {this.state.columnOrder.map((columnId, index) => {
+                const column = this.state.columns[columnId];
+                const tasks = column.taskIds.map(
+                  taskId => this.state.tasks[taskId]
+                );
 
-            return <Column key={column.id} column={column} tasks={tasks} />;
-          })}
-        </Container>
+                return (
+                  <Column
+                    key={column.id}
+                    column={column}
+                    tasks={tasks}
+                    index={index}
+                  />
+                );
+              })}
+            </Container>
+          )}
+        </Droppable>
       </DragDropContext>
     );
   }
--- beautifulDnD/task-app/src/initial-data.js
+++ beautifulDnD/task-app/src/initial-data.js
@@ -15,14 +15,14 @@ const initialData = {
       id: 'column-2',
       title: 'In Progress',
       taskIds: []
-    },
-    'column-3': {
-      id: 'column-3',
-      title: 'Done',
-      taskIds: []
     }
+    // 'column-3': {
+    //   id: 'column-3',
+    //   title: 'Done',
+    //   taskIds: []
+    // }
   },
-  columnOrder: ['column-1', 'column-2', 'column-3']
+  columnOrder: ['column-1', 'column-2']
 };
 
 export default initialData;
