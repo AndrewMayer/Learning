fc4dc3988615530383278920bd78e8d8c954ce08
andrewmayer <andrew@levelupdesign.com>
2019-09-09 17:33:33 -0700

    Working test blog site

---
+++ greatGatsbyBootcamp/gatsby_crash_course/src/pages/2019-08-01/index.md
@@ -0,0 +1,8 @@
+---
+path: "/post-zero"
+date: "2019-08-01"
+title: "My Test Gatsby Post"
+author: "Andrew Mayer"
+---
+
+This is my *test* blog post in Gatsby.
--- greatGatsbyBootcamp/gatsby_crash_course/src/templates/blog-post.js
+++ greatGatsbyBootcamp/gatsby_crash_course/src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "gatsby-link"
 
-export default function Template(data) {
+export default function Template({ data }) {
   const post = data.markdownRemark
   return (
     <div>
